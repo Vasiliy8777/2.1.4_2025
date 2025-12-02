cc800fbb469b1b25bbf3e90b5bfaebcb1cb4cb2c
Vasiliy <exs777@mail.ru>
2025-12-02 21:33:59 +0700

    Commit2

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,12 @@
 package koschei.models;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    @Override
+    public String toString() {
+        return "";
+    }
 }
